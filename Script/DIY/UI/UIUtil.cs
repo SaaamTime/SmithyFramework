022a52665c8ad97e6a47bc43e2d4e80d6e992f44
SaaamTime <914818308@qq.com>
2021-09-07 22:28:48 +0800

    Json name map

--- Script/DIY/UI/UIUtil.cs
+++ Script/DIY/UI/UIUtil.cs
@@ -7,17 +7,15 @@ namespace DIY.UI
     public static class UIUtil
     {
 
-        public static JsonData AutoBindNameTree(Transform _transRoot) {
+        public static JsonData AutoBindNameTree(Transform _transRoot,bool _isChild=false) {
             JsonData nameTree = new JsonData();
             if (_transRoot.childCount > 0)
             {
-                JsonData nameTree_child = new JsonData();
                 for (int i = 0; i < _transRoot.childCount; i++)
                 {
                     Transform child = _transRoot.GetChild(i);
-                    nameTree_child[child.name] = child.childCount > 0 ? AutoBindNameTree(child) : 0;
+                    nameTree[child.name] = child.childCount > 0 ? AutoBindNameTree(child) : 0;
                 }
-                nameTree[_transRoot.name] = nameTree_child;
             }
             return nameTree;
         }
