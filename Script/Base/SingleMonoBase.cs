6dda7de1cb106a6e3e0cbb2cca02d62ad48b839a
SaaamTime <914818308@qq.com>
2022-01-01 19:19:47 +0800

    Add Mono SingleBase

---
+++ Script/Base/SingleMonoBase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SmithyFramework.Base
+{
+    public abstract class SingleMonoBase<T> : MonoBehaviour where T:Component
+    {
+        private static T _instance;
+        public static T Instance {
+            get {
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<T>();
+                }
+                return _instance;
+            }
+        }
+
+        protected SingleMonoBase() { }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
+
